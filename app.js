00bea81ac9a7856f1ecd50df4886ee7e1fbc7995
BMS <saymon.titi@gmail.com>
2020-08-23 14:49:43 -0300

    feat: set headless navigation to false

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: true})
+    const browser = await puppeteer.launch({headless: false})
     const page = await browser.newPage()
     await page.setViewport({ width: 1500, height: 768});
 
