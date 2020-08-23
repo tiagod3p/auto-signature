d63205ef93b8225a03906876875e79f981797a85
BMS <saymon.titi@gmail.com>
2020-08-23 14:57:07 -0300

    feat: go to action required page

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({headless: false})
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1500, height: 768});
+  const browser = await puppeteer.launch({headless: false})
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1500, height: 768});
 
-    await page.goto('https://account-d.docusign.com/')
+  await page.goto('https://account-d.docusign.com/')
 
-    await page.type('#username', 'TYPEYOURUSERNAME@EMAIL.COM')
-    await page.click('.btn.btn-main.btn-lg')
-  
-    await page.waitForNavigation()
-  
-    await page.type('#password', 'TYPEYOURPASSWORD')
-    await page.click('.btn.btn-main.btn-lg')
+  await page.type('#username', 'TYPEYOURUSERNAME@EMAIL.COM')
+  await page.click('.btn.btn-main.btn-lg')
 
+  await page.waitForNavigation()
+
+  await page.type('#password', 'TYPEYOURPASSWORD')
+  await page.click('.btn.btn-main.btn-lg')
+
+  await page.waitForSelector('.title.heading-3.ng-binding')
+  await page.goto('https://appdemo.docusign.com/documents?label=action-required')
+
+  await page.waitForSelector('.m-top-medium.text-center span')
 }) ()
