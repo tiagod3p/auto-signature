59a86f7b25d79d1a65be0ab7797dc0aa95b42731
BMS <saymon.titi@gmail.com>
2020-08-23 15:18:27 -0300

    refactor: insert try catch to close the browser if throws

--- app.js
+++ app.js
@@ -40,31 +40,36 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
 (async () => {
-  const browser = await puppeteer.launch({headless: false})
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1500, height: 768});
-
-  await page.goto('https://account-d.docusign.com/')
-
-  await page.type('#username', 'TYPEYOURUSERNAME@EMAIL.COM')
-  await page.click('.btn.btn-main.btn-lg')
-
-  await page.waitForNavigation()
-
-  await page.type('#password', 'TYPEYOURPASSWORD')
-  await page.click('.btn.btn-main.btn-lg')
-
-  await page.waitForSelector('.title.heading-3.ng-binding')
-  await page.goto('https://appdemo.docusign.com/documents?label=action-required')
-
-  await page.waitForSelector('.m-top-medium.text-center span')
-
   try {
-    await sign(newPage)
+    const browser = await puppeteer.launch({headless: false})
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1500, height: 768});
+  
+    await page.goto('https://account-d.docusign.com/')
+  
+    await page.type('#username', 'TYPEYOURUSERNAME@EMAIL.COM')
+    await page.click('.btn.btn-main.btn-lg')
+  
+    await page.waitForNavigation()
+  
+    await page.type('#password', 'TYPEYOURPASSWORD')
+    await page.click('.btn.btn-main.btn-lg')
+  
+    await page.waitForSelector('.title.heading-3.ng-binding')
+    await page.goto('https://appdemo.docusign.com/documents?label=action-required')
+  
+    await page.waitForSelector('.m-top-medium.text-center span')
+  
+    try {
+      await sign(page)
+    } catch(err) {
+      console.error(err)
+      browser.close()
+    }
+    sleep(5000)
+    browser.close()
   } catch(err) {
     console.error(err)
     browser.close()
   }
-  sleep(5000)
-  browser.close()
 }) ()
